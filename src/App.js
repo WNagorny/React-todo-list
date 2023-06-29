3f28c7ee62ea2be7b95bf136ab835b4dacf6cb0f
WNagorny <walery.nagorny@gmail.com>
2023-06-29 13:06:23 +0200

    styled btn-area

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-     
-     <h1>My todo</h1>
+	const [isComplete, setIsColmplete] = useState(false)
 
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>My todos</h1>
+
+			<div className='todo-wrapper'>
+				<div className='todo-input'>
+					<div className='todo-input-item'>
+						<label>Title</label>
+						<input type='text' placeholder="What's the task title?" />
+					</div>
+
+					<div className='todo-input-item'>
+						<label>Description</label>
+						<input
+							type='text'
+							placeholder="What's the task description?uhguyg"
+						/>
+					</div>
+
+					<div className='todo-input-item'>
+						<button type='button' className='primaryBtn'>
+							Add
+						</button>
+					</div>
+				</div>
+
+				<div className='btn-area'>
+					<button
+						className={`secondaryBtn ${isComplete === false && 'active'}`}
+						onClick={() => setIsColmplete(false)}
+					>
+						Todo
+					</button>
+
+					<button
+						className={`secondaryBtn ${isComplete === true && 'active'}`}
+						onClick={() => setIsColmplete(true)}
+					>
+						Completed
+					</button>
+				</div>
+
+				<div className='todo-list'>
+					<div className='todo-list-item'>
+						<h3>Task 1</h3>
+						<p>Description</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
